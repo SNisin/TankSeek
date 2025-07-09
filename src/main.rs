f92bf17ac7b540d55eee08f25d20034baaebd99b
Sergej Nisin <1641110+SNisin@users.noreply.github.com>
2025-07-09 22:19:48 +0200

    Bi-letter index
    100x speedup. 2x memory

--- src/main.rs
+++ src/main.rs
@@ -133,6 +133,34 @@ fn read_file_list() -> Result<Vec<Element>, Box<dyn Error>> {
     Ok(elements)
 }
 
+fn create_bi_letter_reverse_index(elements: &Vec<Element>) -> HashMap<String, Vec<usize>> {
+    // Create a bi-letter reverse index for the elements
+    let mut index: HashMap<String, Vec<usize>> = HashMap::new();
+    for (i, element) in elements.iter().enumerate() {
+        if element.filename.len() < 2 {
+            continue; // Skip filenames that are too short for bi-letter indexing
+        }
+        // take every two letters of the filename
+        let filename = element.filename.to_lowercase();
+        // be aware of unicode characters, we need to handle them properly
+        let char_vec: Vec<char> = filename.chars().collect();
+        for j in 0..char_vec.len() - 1 {
+            // Create a bi-letter from the current and next character
+            let bi_letter = format!("{}{}", char_vec[j], char_vec[j + 1]);
+            // Insert the index of the element into the index map
+            index.entry(bi_letter).or_default().push(i);
+        }
+    }
+
+    // Ensure indices are unique and sorted
+    for indices in index.values_mut() {
+        indices.dedup(); // Remove duplicates
+        indices.sort_unstable();
+        indices.shrink_to_fit(); // Reduce capacity to the actual number of indices
+    }
+    index
+}
+
 #[macro_use]
 extern crate rocket;
 
@@ -143,18 +171,75 @@ fn index() -> (ContentType, &'static str) {
 }
 
 #[get("/search?<query>")]
-fn search(query: String, elements: &rocket::State<Vec<Element>>) -> String {
+fn search(
+    query: String,
+    elements: &rocket::State<Vec<Element>>,
+    bi_letter_index: &rocket::State<HashMap<String, Vec<usize>>>,
+) -> String {
     let mut results = Vec::new();
     // Normalize the query to lowercase for case-insensitive search
     let query = query.to_lowercase();
-    // Iterate over the records and filter based on the query
-    // limit to 100 results but count all matching records
-    let mut num_results = 0;
-    for record in elements.iter() {
-        if record.filename.to_lowercase().contains(&query) {
+
+    // Check if the query is empty
+    if query.is_empty() {
+        results = elements
+            .iter()
+            .take(100) // Limit to 100 results
+            .cloned()
+            .collect();
+    } else if query.len() < 2 {
+        // If the query is less than 2 characters, TODO
+        return String::from("[]");
+    } else {
+        // Split the query into bi-letters
+        let mut bi_letters = Vec::new();
+        let chars: Vec<char> = query.chars().collect();
+        for i in 0..chars.len() - 1 {
+            // Create a bi-letter from the current and next character
+            let bi_letter = format!("{}{}", chars[i], chars[i + 1]);
+            bi_letters.push(bi_letter);
+        }
+
+        // get the vector of indices for the first bi-letter
+        let mut indices = match bi_letter_index.get(&bi_letters[0]) {
+            Some(indices) => indices.clone(),
+            None => {
+                return String::from("[]");
+            }
+        };
+        // Iterate over the remaining bi-letters and filter the indices
+        for bi_letter in &bi_letters[1..] {
+            if let Some(next_indices) = bi_letter_index.get(bi_letter) {
+                // Only keep indices that are present in both the current indices and the next indices
+                // As both lists are sorted, we can use a two-pointer technique
+                let mut filtered_indices = Vec::new();
+                let mut i = 0;
+                let mut j = 0;
+                while i < indices.len() && j < next_indices.len() {
+                    if indices[i] == next_indices[j] {
+                        filtered_indices.push(indices[i]);
+                        i += 1;
+                        j += 1;
+                    } else if indices[i] < next_indices[j] {
+                        i += 1; // Move to the next index in the current indices
+                    } else {
+                        j += 1; // Move to the next index in the next indices
+                    }
+                }
+                indices = filtered_indices; // Update indices to the filtered list
+            } else {
+                // If no indices found for the current bi-letter, return empty results
+                return String::from("[]");
+            }
+        }
+
+        // Now we have the indices of the elements that match the query
+        let mut num_results = 0; // Counter for the number of results
+        // Prepare the results based on the indices
+        for &index in &indices {
             if num_results < 100 {
                 // If we have less than 100 results, add the record to the results
-                let mut record_with_full_path = record.clone();
+                let mut record_with_full_path = elements[index].clone();
                 // Construct the full path for the record from parents
                 let mut full_path = record_with_full_path.filename.clone();
                 let mut parent_index = record_with_full_path.parent;
@@ -170,17 +255,50 @@ fn search(query: String, elements: &rocket::State<Vec<Element>>) -> String {
                 record_with_full_path.filename = full_path;
                 // Add the record to the results
                 results.push(record_with_full_path);
+                num_results += 1; // Increment the number of results found
             } else {
+                // If we have 100 results, we stop adding more
                 break;
             }
-            // If we have 100 results, we still count the record but don't add it to the results
-            num_results += 1;
         }
+        println!(
+            "Found {} matching records for query '{}'",
+            num_results, query
+        );
     }
-    println!(
-        "Found {} matching records for query '{}'",
-        num_results, query
-    );
+
+    // Iterate over the records and filter based on the query
+    // limit to 100 results but count all matching records
+
+    // let mut num_results = 0;
+    // for record in elements.iter() {
+    //     if record.filename.to_lowercase().contains(&query) {
+    //         if num_results < 100 {
+    //             // If we have less than 100 results, add the record to the results
+    //             let mut record_with_full_path = record.clone();
+    //             // Construct the full path for the record from parents
+    //             let mut full_path = record_with_full_path.filename.clone();
+    //             let mut parent_index = record_with_full_path.parent;
+    //             while parent_index != 0 {
+    //                 if let Some(parent) = elements.get(parent_index) {
+    //                     full_path = format!("{}/{}", parent.filename, full_path);
+    //                     parent_index = parent.parent;
+    //                 } else {
+    //                     break; // If parent not found, break the loop
+    //                 }
+    //             }
+    //             // Update the filename to the full path
+    //             record_with_full_path.filename = full_path;
+    //             // Add the record to the results
+    //             results.push(record_with_full_path);
+    //         } else {
+    //             break;
+    //         }
+    //         // If we have 100 results, we still count the record but don't add it to the results
+    //         num_results += 1;
+    //     }
+    // }
+
     // Convert results to JSON
     match serde_json::to_string(&results) {
         Ok(json) => json,
@@ -195,9 +313,18 @@ fn rocket() -> _ {
         Ok(elements) => {
             println!("Read {} records from filelist.efu", elements.len());
 
+            // Create a bi-letter reverse index for the elements
+            println!("Creating bi-letter reverse index...");
+            let bi_letter_index = create_bi_letter_reverse_index(&elements);
+            println!(
+                "Created bi-letter reverse index with {} entries",
+                bi_letter_index.len()
+            );
+
             //  exit(0); // Exit successfully after reading the file list
             rocket::build()
                 .manage(elements)
+                .manage(bi_letter_index)
                 .mount("/", routes![index, search])
         }
         Err(e) => {
